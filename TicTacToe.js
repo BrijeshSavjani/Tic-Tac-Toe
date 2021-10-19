d0bbab80b8fffbe17c551b3c0f5fdf50f1c64bc0
BrijeshSavjani <21848@mynsb.co.uk>
2021-10-19 04:45:57 +0100

    Game functions
    => MakeMove (Triggered by btn)
    =>ChangeUser(Changes whose turn it is)
    =>CheckWinConditions & CheckDimension (Checks if won)

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,25 +1,51 @@
-let BoardValues = [new Array(3), new Array(3)];
-function MakeMove(id)
-{
-   alert(id);
-}
+let PlayerXTurn = false 
+let PlayerX = []; let Player0 = [];
+class Coordinate{ constructor(id){this.x = id[0];this.y= id[2];}}
 function CreateBoard(Board)
 {
-    for (let row= 0; row < BoardValues[0].length; row++)
+    for (let row= 0; row < 3; row++)
     {
-        
         var Row = document.createElement("tr")
-        for (let column= 0; column < BoardValues[1].length; column++)
+        for (let column= 0; column < 3; column++)
         {
             var Cell = document.createElement("td");
             var CellButton = document.createElement("button");
-            var CellButtonId ="'" + column.toString() + "," +  row.toString() + "'";
-            CellButton.setAttribute("onClick", "MakeMove(" + CellButtonId + ")");
-            Cell.appendChild(CellButton);
-            Row.appendChild(Cell)
+            CellButton.id = row.toString() + "," +  column.toString();
+            CellButton.setAttribute("onClick", "MakeMove( '" + CellButton.id + "')");
+            CellButton.innerText = "";
+            Cell.appendChild(CellButton); Row.appendChild(Cell);
         }
-        Board.appendChild(Row);
+        Board.appendChild(Row);}}
+function ChangeUser(){PlayerXTurn = !PlayerXTurn;}
+function CheckDimension(MovesMade1Dimension)
+{
+    var won = false;
+    MovesMade1Dimension.sort();
+    MovesMade1Dimension.forEach((element,index,array) =>
+    {
+        for(let i =0;i < array.length;i++){
+            try{
+                if (element == array[i] && element == array[i+1]){won = true;}
+                if (Math.abs(element - array[i] - array[i+1]) == 1 ){won = true;}
+            }finally{}
+    }}); 
+    return won;}
+function CheckWinConditions(MovesMade)
+{
+    let MovesMadeX = []; let MovesMadeY = [];
+    var won = false;
+    MovesMade.forEach((element) =>  {MovesMadeX.push(element.x);MovesMadeY.push(element.y);}) ;
+    won = CheckDimension(MovesMadeX) || CheckDimension(MovesMadeY);
+    return won;}
+function MakeMove(id)
+{
+    var CellValue = document.getElementById(id);
+    if (CellValue.innerText == "")
+    {
+        if (PlayerXTurn){CellValue.innerText = "X";PlayerX.push(new Coordinate(id));}
+        else{CellValue.innerText ="0"; Player0.push(new Coordinate(id));}
+        ChangeUser();
+        if (CheckWinConditions(PlayerX) || CheckWinConditions(Player0)){alert("Game Over");}
     }
-    
-}
+    else {alert("Grid is occupied");}}
 
